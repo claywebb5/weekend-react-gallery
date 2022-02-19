e4889f018946d9e14d84952c8c34fe8c78142f57
Clay Webb <clay_webb@outlook.com>
2022-02-19 15:17:35 -0600

    Folders and files created to represent GalleryItem and GalleryList

---
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,2 @@
+// Represents a single image in the gallery with the ability to click the image to
+// toggle between the image and description as well as the ability to like an image
---
+++ src/components/GalleryList/GalleryList.jsx
@@ -0,0 +1,2 @@
+// Represents the gallery of images. By componentizing, 
+// we could reuse this component in different applications
